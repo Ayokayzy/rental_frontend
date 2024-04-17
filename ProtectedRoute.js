e597f37c327526918667603d974a0e09d89ab691
Ayokayzy <theayokayzy1@gmail.com>
2024-04-17 04:06:09 +0100

    update

--- ProtectedRoute.js
+++ ProtectedRoute.js
@@ -1,2 +1,2 @@
-export const API_URL = "https://rental-backend-8mcf.onrender.com";
+export const API_URL = "https://rental-backend-8mcf.onrender.com/api";
 // export const API_URL = "http://localhost:8080/api";
